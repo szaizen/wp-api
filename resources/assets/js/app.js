f6adcd918a39437ad805a473e90c54afcf2a5f37
szaizen <zaizen.saki@gmail.com>
2019-11-20 18:28:45 +0900

    記事取得してタイトル表示

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,9 +1,30 @@
-import Sample from './sample'
+const url = "https://liginc.co.jp";
+const api_url = url + "/wp-json/wp/v2/posts";
 
-const sample = new Sample({
-  elements: [ ...document.querySelectorAll('.js-target') ],
-  classNameActive: 'is-active'
-})
-setTimeout(() => {
-  sample.activate()
-}, 500)
+requestAjax(api_url, function(response) {
+  addCard(response);
+});
+
+function requestAjax(endpoint, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      callback(this.response);
+    }
+  };
+  xhr.responseType = "json";
+  xhr.open("GET", endpoint, true);
+  xhr.send();
+}
+
+function addCard(response) {
+  var $template = document.getElementsByClassName("template")[0];
+  var $add = document.getElementsByClassName("add")[0];
+
+  for (var i = 0; i < 10; i++) {
+    let clone = $template.firstElementChild.cloneNode(true);
+    clone.getElementsByClassName("article__title")[0].innerText =
+      response[i].title.rendered;
+    $add.appendChild(clone);
+  }
+}
