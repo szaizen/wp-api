9712428ca5e2efff48febc98f813298136198427
szaizen <zaizen.saki@gmail.com>
2019-11-25 17:14:50 +0900

    APIのURLを取得する関数を外出し

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,18 +1,23 @@
 import formatData from "./modules/format-data.js";
 import createDom from "./modules/create-dom.js";
 import { articleTotal, pagesTotal, requestApi } from "./modules/request-api.js";
+import getApiUrl from "./modules/get-api-url.js";
 
 const $add = document.getElementById("js-add");
 const $pagenation = document.getElementById("js-pagination");
 
 let categoryList = [];
-let currentPage = 1; // 現在のページ番号
+
+export let currentType = "post"; // post or category or search
+export let currentPage = 1; // 現在のページ番号
+export let searchText = "";
+export let cateogyrId = "";
 
 // API URL
-const URL = "https://liginc.co.jp";
-const ARTICLE_URL = URL + "/wp-json/wp/v2/posts?_embed&per_page=9";
-const CATEGORY_URL = URL + "/wp-json/wp/v2/categories?per_page=100";
-// let pageUrl = ARTICLE_URL + "page=" + currentPage;
+export const API_URL =
+  "https://liginc.co.jp/wp-json/wp/v2/posts?_embed&per_page=9";
+const CATEGORY_URL =
+  "https://liginc.co.jp/wp-json/wp/v2/categories?per_page=100";
 
 // カテゴリー取得、サイドバー更新
 requestApi(CATEGORY_URL).then(result => {
@@ -23,12 +28,7 @@ requestApi(CATEGORY_URL).then(result => {
 });
 
 // 記事一覧
-requestApi(ARTICLE_URL).then(result => {
-  addCard(result);
-});
-
-// ページネーション
-addPagenaition(currentPage);
+addCard();
 
 /* ------------------------------------
  * クリックイベント
@@ -36,31 +36,26 @@ addPagenaition(currentPage);
 
 // 検索時
 document.getElementById("js-search-btn").addEventListener("click", () => {
-  let searchUrl =
-    ARTICLE_URL + "&search=" + document.getElementById("js-search-text").value;
-  searchUrl = encodeURI(searchUrl);
-
-  requestApi(searchUrl).then(result => {
-    addCard(result);
-  });
+  currentType = "search";
+  currentPage = 1;
+  searchText = document.getElementById("js-search-text").value;
+  addCard();
 });
 
 // カテゴリーボタン
 async function categorySearch(id) {
-  let categoryListURL = ARTICLE_URL + "&categories=" + id;
-  const result = await requestApi(categoryListURL);
-  addCard(result);
+  currentType = "category";
+  addCard();
 }
 
 // ページネーション
 $pagenation.addEventListener("click", e => {
   let clickPage = Number(e.target.dataset.pagenumber);
-  addPagenaition(clickPage);
 
-  let pageUrl = ARTICLE_URL + "&page=" + clickPage;
-  requestApi(pageUrl).then(result => {
-    addCard(result);
-  });
+  currentPage = clickPage;
+  addCard();
+
+  addPagenaition(clickPage);
 });
 
 /* ------------------------------------
@@ -68,9 +63,13 @@ $pagenation.addEventListener("click", e => {
  *------------------------------------*/
 
 // 記事追加
-function addCard(json) {
-  document.getElementById("js-add").textContent = null;
+async function addCard() {
+  let url = encodeURI(getApiUrl());
+  const json = await requestApi(url);
 
+  addPagenaition(currentPage);
+
+  document.getElementById("js-add").textContent = null;
   if (json.length === 0) {
     showError("該当する記事はありませんでした");
   } else {
@@ -99,11 +98,11 @@ function addSidebarCategoryList() {
     .map(item => `<li data-categoryid="${item.id}">${item.name}</li>`)
     .join("");
   $ul.addEventListener("click", e => {
-    categorySearch(e.target.dataset.categoryid);
+    cateogyrId = e.target.dataset.categoryid;
+    currentPage = 1;
+    categorySearch();
   });
 }
-
-// API取得
 
 // ページネーション更新
 function addPagenaition(currentPageNumber) {
@@ -113,8 +112,12 @@ function addPagenaition(currentPageNumber) {
   if (currentPageNumber <= 2) {
     pagenationStartNumber = 1;
     pagenationEndNumber = 5;
-  } else if (currentPageNumber >= pagesTotal - 2) {
+  } else if (currentPageNumber >= pagesTotal - 2 && currentPageNumber >= 5) {
     pagenationStartNumber = pagesTotal - 4;
+    pagenationEndNumber = pagesTotal;
+  }
+
+  if (pagesTotal < 5) {
     pagenationEndNumber = pagesTotal;
   }
 
